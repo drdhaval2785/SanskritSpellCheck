abed16c85c97f6ca9129a6973badd8de7c734168
drdhaval2785 <drdhaval2785@gmail.com>
2014-10-08 23:22:05 +0530

    some regex

--- countvowels.php
+++ countvowels.php
@@ -5,11 +5,22 @@ ini_set('max_execution_time', 36000);
 ini_set("memory_limit","1000M");
 include "dev-slp.php";
 include "slp-dev.php";
+$az=array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",);
+$digit=array("1","2","3","4","5","6","7","8","9","0",);
 $ac = array("a","A","i","I","u","U","f","F","x","X","e","o","E","O",); // vowels - 'ac' letters
 $hl = array("k","K","g","G","N","c","C","j","J","Y","w","W","q","Q","R","t","T","d","D","n","p","P","b","B","m","y","r","l","v","S","z","s","h","M","H"); 
 $al = array("a","A","i","I","u","U","f","F","x","X","e","o","E","O","k","K","g","G","N","c","C","j","J","Y","w","W","q","Q","R","t","T","d","D","n","p","P","b","B","m","y","r","l","v","S","z","s","h","M","H");
-$file=file_get_contents("meghadhuta-CVC-SLP1.txt");
-$file=str_replace(array("1","2","3","4","5","6","7","8","9","0",".","'","?",",","[","]"," ","/"),array("","","","","","","","","","","","","","","","","",""),$file);
+$file=file_get_contents("abc.txt");
+$file=str_replace(array("1","2","3","4","5","6","7","8","9","0",".","'","?",",","[","]"," ","/","_","*"),array("","","","","","","","","","","","","","","","","","","",""),$file);
+foreach($az as $val1)
+{
+    foreach  ($digit as $val2)
+    {
+        $file=str_replace($val2."ab",$val2,$file);
+        $file=str_replace($val2."cd",$val2,$file);
+        $file=str_replace($val2.$val1." ",$val2,$file);
+    }
+}
 $file=str_replace("\r","",$file);
 $file=str_replace("<br>","",$file);
 $file=str_replace("\n","",$file);
