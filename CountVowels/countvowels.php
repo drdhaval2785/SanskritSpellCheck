203ea957e05c675485a88ab2a2721b47d150ab06
drdhaval2785 <drdhaval2785@gmail.com>
2014-11-30 14:13:12 +0530

    rounding off the figures to 2 decimals
    https://github.com/drdhaval2785/SanskritSpellCheck/issues/5#issuecomment-58450754
    point 2 cleared

--- CountVowels/countvowels.php
+++ CountVowels/countvowels.php
@@ -3,14 +3,14 @@
 ini_set('max_execution_time', 36000);
 /* set memory limit to 1000 MB */
 ini_set("memory_limit","1000M");
-include "dev-slp.php";
-include "slp-dev.php";
+//include "dev-slp.php";
+//include "slp-dev.php";
 $az=array("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",);
 $digit=array("1","2","3","4","5","6","7","8","9","0",);
 $ac = array("a","A","i","I","u","U","f","F","x","X","e","o","E","O",); // vowels - 'ac' letters
 $hl = array("k","K","g","G","N","c","C","j","J","Y","w","W","q","Q","R","t","T","d","D","n","p","P","b","B","m","y","r","l","v","S","z","s","h","M","H"); 
 $al = array("a","A","i","I","u","U","f","F","x","X","e","o","E","O","k","K","g","G","N","c","C","j","J","Y","w","W","q","Q","R","t","T","d","D","n","p","P","b","B","m","y","r","l","v","S","z","s","h","M","H");
-$file=file_get_contents("abc.txt");
+$file=file_get_contents("meghadhuta-CVC-SLP1.txt");
 $file=str_replace(array("1","2","3","4","5","6","7","8","9","0",".","'","?",",","[","]"," ","/","_","*"),array("","","","","","","","","","","","","","","","","","","",""),$file);
 foreach($az as $val1)
 {
@@ -25,12 +25,14 @@ $file=str_replace("\r","",$file);
 $file=str_replace("<br>","",$file);
 $file=str_replace("\n","",$file);
 $file=trim($file);
-echo $file."<br>";
+//echo $file."<br>";
 $split=preg_split('/([aAiIuUfFxXeEoO])/',$file,0,PREG_SPLIT_DELIM_CAPTURE);
 echo "Occurrence of vowels - ".((count($split)-1)/2)."<br>";
 $split1=preg_split('/([kKgGNcCjJYwWqQRtTdDnpPbBmyrlvSzshMH])/',$file,0,PREG_SPLIT_DELIM_CAPTURE);
 echo "Occurrence of consonants - ".((count($split1)-1)/2)."<br>";
 $split1=preg_split('/([kKgGNcCjJYwWqQRtTdDnpPbBmyrlvSzshMH])/',$file,0,PREG_SPLIT_DELIM_CAPTURE);
-echo "Ratio of consonants per 100 vowels -".(count($split1)*100)/count($split)."<br>";
+$figure = (count($split1)*100)/count($split);
+$roundedfigure = round($figure,2);
+echo "Ratio of consonants per 100 vowels -".$roundedfigure."<br>";
 
 ?>
