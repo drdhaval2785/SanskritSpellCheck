b9ea7f6a2c3ed41dda41872b31513e983f9800a4
Dr. Dhaval Patel <drdhaval2785@gmail.com>
2015-11-15 23:00:55 +0530

    updated o_vs_O based on new sanhw2

--- o_vs_O/composite.sh
+++ o_vs_O/composite.sh
@@ -1,9 +1,9 @@
 # php o_vs_O_sanhw2.php takes a lot of time. 
 # Therefore, use only o_vs_O2.txt preferrably. 
 # You would want to regenerate o_vs_O2.txt only if sanhw2.txt is altered.
-#echo "Running o_vs_O_sanhw2.php and storing in o_vs_O2.txt."
-#echo 
-#php o_vs_O_sanhw2.php
+echo "Running o_vs_O_sanhw2.php and storing in o_vs_O2.txt."
+echo 
+php o_vs_O_sanhw2.php
 echo "Running dictsorting_single.php."
 echo 
 php dictsorting_single.php
