e0a912e9c49372fa353ca927349b10d384cd8366
drdhaval2785 <drdhaval2785@gmail.com>
2017-09-03 16:29:02 +0530

    whitelist words added to nochange folder

---
+++ chg_nchg_sep.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# python chg_nchg_sep.py AllvsMW/AllvsMW_sf_corrected.txt AllvsMW/chg.txt AllvsMW/nchg.txt
+import codecs, re, sys
+if __name__=="__main__":
+	filein = sys.argv[1]
+	filechange = sys.argv[2]
+	filenochange = sys.argv[3]
+	fin = codecs.open(filein,'r','utf-8')
+	fchg = codecs.open(filechange,'w','utf-8')
+	fnchg = codecs.open(filenochange,'w','utf-8')
+	for line in fin:
+		if line.endswith(':n\n'):
+			splt = line.split(':')
+			wrd = splt[1]
+			fnchg.write(wrd+'\n')
+		else:
+			fchg.write(line)
+	fin.close()
+	fchg.close()
+	fnchg.close()
+	
